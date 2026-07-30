8cbdb4bc63bb1ec730641241f779cbd22cee842c
MicheleFilisina <michifilip2006@gmail.com>
2026-07-30 22:09:39 +0200

    Add initial implementation of Chip8 emulator and guidelines

---
+++ chip8.cpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+
+const uint16_t start_address = 0x200;
+
+class Chip8
+{
+public:
+	uint8_t registers[16]{};
+	uint8_t memory[4096]{};
+	uint16_t index{};
+	uint16_t pc{};
+	uint16_t stack[16]{};
+	uint8_t sp{};
+	uint8_t delayTimer{};
+	uint8_t soundTimer{};
+	uint8_t keypad[16]{};
+	uint32_t video[64 * 32]{};
+	uint16_t opcode;
+	void Initialize();
+};
+
+void Chip8::Initialize(){
+
+}
